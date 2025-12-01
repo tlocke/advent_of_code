d9c9cb4cda21d736760d7966068a7e3b8eaf4323
Tony Locke <tlocke@tlocke.org.uk>
2025-12-01 21:16:56 +0000

    Added Day 1, Part 1

--- 2025/day_1/src/main.rs
+++ 2025/day_1/src/main.rs
@@ -1,8 +1,8 @@
+use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use std::fs::File;
 
-fn main() {
+fn part_1() {
     let path = Path::new("input.txt");
     let file = File::open(&path).expect("Can't find file");
     let reader = io::BufReader::new(file);
@@ -28,7 +28,62 @@ fn main() {
             zeros += 1;
         }
         println!("zeros {zeros}");
-
     }
+}
 
+fn next_dial(dial: &i64, is_forward: &bool) -> i64 {
+    match (dial, is_forward) {
+        (99, true) => 0,
+        (0, false) => 99,
+        (_, true) => dial + 1,
+        (_, false) => dial - 1,
+    }
+}
+
+fn part_2(file_name: &str) -> i64 {
+    let path = Path::new(file_name);
+    let file = File::open(&path).expect("Can't find file");
+    let reader = io::BufReader::new(file);
+    let mut dial: i64 = 50;
+    let mut zeros: i64 = 0;
+    for line in reader.lines() {
+        let line_str = line.unwrap();
+
+        let chars: Vec<char> = line_str.chars().collect();
+        let is_forward: bool = match chars.first().unwrap() {
+            'R' => true,
+            'L' => false,
+            _ => panic!("Direct must be 'L' or 'R'."),
+        };
+
+        let click_slice = &chars[1..];
+        let click_str: String = click_slice.iter().collect();
+        let clicks: i64 = click_str.parse().unwrap();
+
+        for _ in 0..clicks {
+            let new_dial = next_dial(&dial, &is_forward);
+            if new_dial == 0 {
+                zeros += 1;
+            }
+            dial = new_dial;
+        }
+    }
+    zeros
+}
+
+fn main() {
+    part_1();
+    let zeros = part_2("input.txt");
+    println!("part 2 {zeros}");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_2_example() {
+        let zeros = part_2("example.txt");
+        assert_eq!(zeros, 6);
+    }
 }
